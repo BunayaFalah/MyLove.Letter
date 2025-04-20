c645f5bd04d0185e063a110d4b736353ab48672e
BunayaFalah <alfalahbunaya@gmail.com>
2025-04-20 20:09:17 +0700

    Putri Salju

---
+++ script.js
@@ -0,0 +1,5 @@
+function showLoveMessage() {
+    const message = document.getElementById('loveMessage');
+    message.innerText = "Putri, Kamu Adalah Satu Satunya Perempuan yang Aku Pilih Untuk Selamanya, Mari Kita Usahakan dan Berusaha Bersama Untuk Menuju Akad Pernikahan ya Cintaku Honeyku❤️";
+    message.classList.remove('hidden');
+}
